257a69dc1047f0a60fb15ce7d6c93b07b4b457d2
matt <matt@atlasagency.com.au>
2021-02-25 10:58:28 +1100

    Update: post section

---
+++ js/master.js
@@ -0,0 +1,26 @@
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
+import $ from "jquery";
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  //   console.log(999);
+  utils.initImageModal();
+});
+
+var utils = (function (params) {
+  function initImageModal() {
+    var $popModal = $("#popModal");
+    if ($popModal) {
+      // check page for element
+      $(".js-pop-modal").click(function (params) {
+        var img = $(this).attr("src");
+
+        $popModal.find(".js-pop-target").attr("src", img);
+        $popModal.modal("toggle");
+      });
+    }
+  }
+
+  return {
+    initImageModal: initImageModal,
+  };
+})();
