a4bc330d2efd837409091c1a5e57e83828867558
matt <matt@atlasagency.com.au>
2021-03-01 15:20:23 +1100

    Update: move files for semplicyt

---
+++ src/js/vendor/bs4.js
@@ -0,0 +1,36 @@
+// import "../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min"; // this should be atomized, however single imports do not work with ie11
+
+// import Alert from "../../../node_modules/bootstrap/js/src/alert";
+// import Button from "../../../node_modules/bootstrap/js/src/button";
+// import Carousel from "../../../node_modules/bootstrap/js/src/carousel";
+// import Collapse from "../../../node_modules/bootstrap/js/src/collapse";
+// import Dropdown from "../../../node_modules/bootstrap/js/src/dropdown";
+import Modal from "../../../node_modules/bootstrap/js/src/modal";
+// import Popover from "../../../node_modules/bootstrap/js/src/popover";
+// import Scrollspy from "../../../node_modules/bootstrap/js/src/scrollspy";
+// import Tab from "../../../node_modules/bootstrap/js/src/tab";
+// import Toast from "../../../node_modules/bootstrap/js/src/toast";
+// import Tooltip from "../../../node_modules/bootstrap/js/src/tooltip";
+// import Util from "../../../node_modules/bootstrap/js/src/util";
+
+// /**
+//  * --------------------------------------------------------------------------
+//  * Bootstrap (v4.6.0): index.js
+//  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+//  * --------------------------------------------------------------------------
+//  */
+
+export {
+  //   Util,
+  //   Alert,
+  //   Button,
+  //   Carousel,
+  //   Collapse,
+  //   Dropdown,
+  Modal,
+  //   Popover,
+  //   Scrollspy,
+  //   Tab,
+  //   Toast,
+  //   Tooltip,
+};
